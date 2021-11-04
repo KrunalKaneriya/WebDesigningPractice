c36eb7f2053b6540859b2b4d123a29390a3c1369
=Krunal Kaneria <=prokillerkk@outlook.com>
2021-11-04 20:23:36 +0530

    Added Html File in GitIgnore

---
+++ new_javascript_features.js
@@ -0,0 +1,23 @@
+//*********************************Default Parameters*********************************/
+//*We need to take care of the ordering of default parameters because if we have only one param
+//*then the function is going to replace it with msg not person so it will display undefined
+//*in person variable
+const hello = (msg = "Hey There", person) => {
+    console.log(`${msg} ${person}`);
+}
+const greet = (person,msg = "Good Morning") => {
+    console.log(`${msg} ${person}`)
+}
+
+//****************************Spread With Function Calls *******************************/
+//*Spread is new feature of javascript which allows to pass a array or object or string which
+//*contain multiple data in one variable to be called diffrently via just using just the variable
+//*not via traversing each element
+
+const nums = [4545,5471,4416461,3213,546,13,16,541,1354300,103,21];
+//!console.log(Math.min(nums)); //If we use this then it will display NaN
+//*Here the single array is spread out and the function is called with each element of array just 
+//*like for each
+console.log(Math.min(...nums));
+console.log(Math.max(...nums));
+console.log(..."Hello World");
