729961526a59a7f90457452efe39925af46b5186
=Krunal Kaneria <=prokillerkk@outlook.com>
2021-11-25 16:16:44 +0530

    Created First Node JS File

--- NodeJS/firstScript.js
+++ NodeJS/firstScript.js
@@ -1,3 +1,9 @@
 for(let i=0;i<10;i++) {
-    console.log("Hello World");
+    console.log("I AM USING NODE JS");
+}
+
+function timeOut() {
+    setTimeout(() => {
+        console.log("One Second is Passed...");
+    },1000)
 }
